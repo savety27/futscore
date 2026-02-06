12af8d746194603b540770c33e1e900303574ee5
ghstx9 <sorrowprivategt@gmail.com>
2026-02-06 15:24:55 +0700

    refine design v2

--- includes/config.php
+++ includes/config.php
@@ -5,8 +5,17 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'futscore_db');
 
-// Site configuration
-define('SITE_URL', 'http://localhost/futscore');
+// Site configuration (supports ngrok/reverse proxies)
+$protocol = 'http';
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $protocol = 'https';
+} elseif (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $protocol = 'https';
+}
+
+$host = $_SERVER['HTTP_X_FORWARDED_HOST'] ?? $_SERVER['HTTP_HOST'] ?? 'localhost';
+$path = str_replace(basename($_SERVER['PHP_SELF']), '', $_SERVER['PHP_SELF']);
+define('SITE_URL', rtrim($protocol . '://' . $host . $path, '/'));
 define('SITE_NAME', 'Futscore');
 define('SITE_DESC', 'Aplikasi berbasis web untuk pembinaan atlet futsal');
 
