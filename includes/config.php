5525dfe94f5168e5063952233ddbc1c540a0ea03
Cemetheus <masitahco@gmail.com>
2026-02-14 13:48:35 +0700

    p

--- includes/config.php
+++ includes/config.php
@@ -16,7 +16,7 @@ if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
 $host = $_SERVER['HTTP_X_FORWARDED_HOST'] ?? $_SERVER['HTTP_HOST'] ?? 'localhost';
 $path = str_replace(basename($_SERVER['PHP_SELF']), '', $_SERVER['PHP_SELF']);
 define('SITE_URL', rtrim($protocol . '://' . $host . $path, '/'));
-define('SITE_NAME', 'Futscore');
+define('SITE_NAME', 'Alvetrix');
 define('SITE_DESC', 'Aplikasi berbasis web untuk pembinaan atlet futsal');
 
 // Timezone
