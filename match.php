69ccd098eac22cda5514a65ce05579650f6a133b
ghstx9 <sorrowprivategt@gmail.com>
2026-02-21 06:16:42 +0700

    added active class to the challenge button in match.php

--- match.php
+++ match.php
@@ -237,7 +237,7 @@ if (!$matchNotFound) {
         <nav class="sidebar-nav">
             <a href="<?php echo SITE_URL; ?>"><i class="fas fa-home"></i> <span>BERANDA</span></a>
             <a href="EVENTS.PHP"><i class="fas fa-calendar-alt"></i> <span>EVENT</span></a>
-            <a href="all.php"><i class="fas fa-trophy"></i> <span>CHALLENGE</span></a>
+            <a href="all.php" class="active"><i class="fas fa-trophy"></i> <span>CHALLENGE</span></a>
             <a href="team.php"><i class="fas fa-users"></i> <span>TIM</span></a>
             <div class="nav-item-dropdown">
                 <a href="#" class="nav-has-dropdown" onclick="toggleDropdown(this, 'playerDropdown'); return false;">
